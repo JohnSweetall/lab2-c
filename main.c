419e42b5725695459e1b3bf832cd5e4b5f9be2bd
JohnSweetall <jts6052@psu.edu>
2020-09-08 15:51:50 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,45 @@
+// Author: John Sweetall jts6052@psu.edu
+// Collaborator: Caroline Brustoloni cxb5766@psu.edu
+// Collaborator: Siddhant Rajoriya sbr5632@psu.edu
+// Collaborator: Saeed Alshebli saa6016@psu.edu
+// Section: 2
+// Breakout: 11
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+
+int main(void) {
+
+char *part1 = readline("Enter your CMPSC 131 grade: ");
+double grade = atof(part1);
+
+if (grade >= 93.0) {
+  printf("Your letter grade for CMPSC 131 is A.");
+}
+else if (grade >= 90.0 ) {
+  printf("Your letter grade for CMPSC 131 is A-.");
+}
+else if (grade >= 87.0) {
+  printf("Your letter grade for CMPSC 131 is B+.");
+}
+else if (grade >= 83.0) {
+  printf("Your letter grade for CMPSC 131 is B.");
+}
+else if (grade >= 80.0) {
+  printf("Your letter grade for CMPSC 131 is B-.");
+}
+else if (grade >= 77.0) {
+  printf("Your letter grade for CMPSC 131 is C.");
+}
+else if (grade >= 70.0) {
+  printf("Your letter grade for CMPSC 131 is C-.");
+}
+else if (grade >= 60.0) {
+  printf("Your letter grade for CMPSC 131 is D.");
+}
+else if (grade < 60.0) {
+  printf("Your letter grade for CMPSC 131 is F.");
+}
+
+}
