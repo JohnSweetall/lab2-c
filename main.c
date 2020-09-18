f8a79e8602bfb3f107d3a083a930e00f65d2757e
JohnSweetall <jts6052@psu.edu>
2020-09-18 02:52:47 +0000

    spacing

--- main.c
+++ main.c
@@ -17,27 +17,35 @@ double grade = atof(part1);
 if (grade >= 93.0) {
   printf("Your letter grade for CMPSC 131 is A.");
 }
+
 else if (grade >= 90.0 ) {
   printf("Your letter grade for CMPSC 131 is A-.");
 }
+
 else if (grade >= 87.0) {
   printf("Your letter grade for CMPSC 131 is B+.");
 }
+
 else if (grade >= 83.0) {
   printf("Your letter grade for CMPSC 131 is B.");
 }
+
 else if (grade >= 80.0) {
   printf("Your letter grade for CMPSC 131 is B-.");
 }
+
 else if (grade >= 77.0) {
   printf("Your letter grade for CMPSC 131 is C+.");
 }
+
 else if (grade >= 70.0) {
   printf("Your letter grade for CMPSC 131 is C.");
 }
+
 else if (grade >= 60.0) {
   printf("Your letter grade for CMPSC 131 is D.");
 }
+
 else if (grade < 60.0) {
   printf("Your letter grade for CMPSC 131 is F.");
 }
